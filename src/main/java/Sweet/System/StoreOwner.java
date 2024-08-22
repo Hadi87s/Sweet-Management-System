74d7497019f39cec0e73d94d731d4491e4c3cba0
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2024-08-22 19:46:49 +0300

    Refactoring and Checking of the Coverage percentage.

--- src/main/java/Sweet/System/StoreOwner.java
+++ src/main/java/Sweet/System/StoreOwner.java
@@ -60,26 +60,32 @@ public class StoreOwner extends User{
         AddressChanged = false;
     }
 
+    @Override
     public String getUsername() {
         return username;
     }
 
+    @Override
     public void setUsername(String username) {
         this.username = username;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
 
+    @Override
     public void setPassword(String password) {
         this.password = password;
     }
 
+    @Override
     public String getEmail() {
         return email;
     }
 
+    @Override
     public void setEmail(String email) {
         this.email = email;
     }
