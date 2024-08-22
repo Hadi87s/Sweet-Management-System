cc8aceeedc9e6e0dc4fd9960776ac42eb30c3ce8
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2024-08-22 20:01:11 +0300

    Removing Duplicated Code.

--- src/main/java/Sweet/System/StoreOwner.java
+++ src/main/java/Sweet/System/StoreOwner.java
@@ -60,30 +60,6 @@ public class StoreOwner extends User{
         AddressChanged = false;
     }
 
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
 
     @Override
     public void setEmail(String email) {
